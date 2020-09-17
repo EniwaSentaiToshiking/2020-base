2b78b0ba291132a0fc644ea454e6b8cfe14999a6
hama-matcha <hamamatcha@gmail.com>
2020-09-17 19:15:12 +0900

    (add) LED debug

--- app.cpp
+++ app.cpp
@@ -2,24 +2,40 @@
 #include "Tracer.h" // <2>
 #include "Clock.h"  // <3>
 #include "Straight.h"
+#include "WaitStart.h"
 
 using namespace ev3api;
 
 Tracer tracer;  // <4>
 Clock clock;    // <5>
 Straight straight;
+WaitStart waitStart;
 
-void tracer_task(intptr_t exinf) { // <1>
+void tracer_task(intptr_t exinf) 
+{ // <1>
   tracer.run(); // <2>
   ext_tsk();
 }
 
-void straight_task(intptr_t exinf) {
+void straight_task(intptr_t exinf) 
+{
   const uint32_t duration = 2000;
   straight.forward();
   clock.sleep(duration);
   straight.backward();
   clock.sleep(duration);
+}
+
+void wait_start_task(intptr_t exinf) 
+{
+  sta_cyc(WAIT_START_CYC);
+  ev3_led_set_color(LED_GREEN);
+  if (ev3_button_is_pressed(LEFT_BUTTON))
+  {
+    ev3_led_set_color(LED_RED);
+    // act_tsk(MAIN_TASK);
+    // stp_cyc(WAIT_START_CYC);
+  }
 }
 
 void main_task(intptr_t unused) { // <1>
