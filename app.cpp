9803006df204a87e7de4dc351c37c0f1c37c671c
hama-matcha <hamamatcha@gmail.com>
2020-09-22 17:30:18 +0900

    (fix) Task priority

--- app.cpp
+++ app.cpp
@@ -1,21 +1,13 @@
 #include "app.h" 
 #include "SetupGame.h"
-#include "Clock.h" 
 #include "GameStateManager.h"
 
 using namespace ev3api;
 
 SetupGame setupGame;
 GameStateManager gameStateManager;
-Clock clock;
+
 int nowState = RUNNING_GAME_STATE;
-
-void game_state_manager_task(intptr_t exinf)
-{
-  gameStateManager.manageGameState();
-  ext_tsk();
-}
-
 void setup_game_task(intptr_t exinf)
 {
   if (setupGame.isStarted())
@@ -29,5 +21,11 @@ void setup_game_task(intptr_t exinf)
     stp_cyc(GAME_STATE_MANAGER_CYC);
     gameStateManager.terminate();
   }
+  ext_tsk();
+}
+
+void game_state_manager_task(intptr_t exinf)
+{
+  gameStateManager.manageGameState();
   ext_tsk();
 }
--- app.h
+++ app.h
@@ -4,8 +4,8 @@ extern "C" {
 
 #include "ev3api.h"
 
-#define MAIN_PRIORITY    (TMIN_APP_TPRI + 1)
-#define TRACER_PRIORITY  (TMIN_APP_TPRI + 2)
+#define SETUP_GAME_PRIORITY (TMIN_APP_TPRI + 1)
+#define GAME_STATE_MANAGER_PRIORITY (TMIN_APP_TPRI + 2)
 
 #ifndef STACK_SIZE
 #define STACK_SIZE      (512)
@@ -13,10 +13,10 @@ extern "C" {
 
 #ifndef TOPPERS_MACRO_ONLY
 
-extern void game_state_manager_task(intptr_t exinf);
-extern void game_state_manager_cyc(intptr_t exinf);
-extern void setup_game_task(intptr_t exinf);
-extern void setup_game_cyc(intptr_t exinf);
+    extern void setup_game_task(intptr_t exinf);
+    extern void setup_game_cyc(intptr_t exinf);
+    extern void game_state_manager_task(intptr_t exinf);
+    extern void game_state_manager_cyc(intptr_t exinf);
 
 #endif /* TOPPERS_MACRO_ONLY */
 
