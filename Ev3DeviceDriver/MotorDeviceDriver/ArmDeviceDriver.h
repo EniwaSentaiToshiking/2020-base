0d3ced3b4ce7b663990d06039985154ca1794555
hama-matcha <hamamatcha@gmail.com>
2020-10-25 13:29:33 +0900

    (add) arm device driver

---
+++ Ev3DeviceDriver/MotorDeviceDriver/ArmDeviceDriver.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include "Motor.h"
+#include "DebugUtil.h"
+#include <math.h>
+#include <stdlib.h>
+
+using namespace ev3api;
+
+class ArmDeviceDriver
+{
+    Motor armMotor;
+
+public:
+    ArmDeviceDriver();
+    void init();
+    void resetArmAngle();
+    void rotate(int32_t angle);
+    void setPWM(int armPWM);
+    void rotateDefault();
+    void terminate();
+    ~ArmDeviceDriver();
+
+private:
+    const int DEFAULT_ANGLE = -55;
+
+    const int KP = 2.5F;
+    const int PWM_ABS_MAX = 60;
+    /* debug */
+    DebugUtil d;
+    FILE *file;
+};
