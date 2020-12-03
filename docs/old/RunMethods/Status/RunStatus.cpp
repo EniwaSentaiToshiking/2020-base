641ae8518356cb0809354d62436049f745716389
hama-matcha <hamamatcha@gmail.com>
2020-12-03 17:03:22 +0900

    (add) docs model file

---
+++ docs/old/RunMethods/Status/RunStatus.cpp
@@ -0,0 +1,40 @@
+#include "RunStatus.h"
+#include "RunManager.h"
+
+RunStatus::~RunStatus()
+{
+    for_each(patterns.begin(), patterns.end(), DeleteObject());
+    patterns.clear();
+}
+
+bool RunStatus::run()
+{
+    bool isFinishPattern = patterns[currentPattern]->run();
+    if (isFinishPattern)
+    {
+        bool isFinishStatus = changeNextPattern();
+        if (isFinishStatus)
+            return true;
+    }
+    return false;
+}
+
+bool RunStatus::changeNextPattern()
+{
+    // RunPatternが全て正しく動作しきったら 次のstateに移す
+    if (currentPattern < patterns.size() - 1)
+    {
+        currentPattern++;
+        ev3_speaker_play_tone(480, 100);
+    }
+    else
+    {
+        return true;
+    }
+    return false;
+}
+
+void RunStatus::changeNextStatus(RunManager *manager)
+{
+    manager->changeStatus(this->nextState);
+}
